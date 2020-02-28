f1915967160f2fdbbb660d46062368536643e7ac
alfintashim <alfin.6661@gmail.com>
2020-02-28 13:01:11 +0700

    add dom pdf

--- config/app.php
+++ config/app.php
@@ -165,6 +165,7 @@ return [
         Yajra\DataTables\DataTablesServiceProvider::class,
         RealRashid\SweetAlert\SweetAlertServiceProvider::class,
         Collective\Html\HtmlServiceProvider::class,
+        Barryvdh\DomPDF\ServiceProvider::class,
 
         /*
          * Package Service Providers...
@@ -234,6 +235,7 @@ return [
         'Alert' => RealRashid\SweetAlert\Facades\Alert::class,
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
+        'PDF' => Barryvdh\DomPDF\Facade::class,
 
         'LogSurat' => App\Helpers\LogSurat::class,
 
